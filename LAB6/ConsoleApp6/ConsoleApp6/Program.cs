21a79177a6f7cf74259e4899367048cb8fa95a5f
Alexandr Demidenko <148003381+soManyVoices@users.noreply.github.com>
2023-12-23 15:18:42 +0300

    Add files via upload

---
+++ LAB6/ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using RepairShopLibrary;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Создание экземпляра мастерской
+        RepairJob.RepairShop repairShop = new RepairJob.RepairShop();
+
+        while (true)
+        {
+            Console.WriteLine("Выберите действие:");
+            Console.WriteLine("1. Добавить ремонтную работу");
+            Console.WriteLine("2. Получить общее количество выполненных работ за период");
+            Console.WriteLine("3. Получить среднее количество работ в день для указанного типа работ");
+            Console.WriteLine("4. Получить наиболее востребованный вид работ за период");
+            Console.WriteLine("5. Вывести список всех добавленных работ");
+            Console.WriteLine("6. Выйти из приложения");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Введите дату ремонтной работы (в формате ГГГГ-ММ-ДД):");
+                    DateTime date;
+                    if (DateTime.TryParse(Console.ReadLine(), out date))
+                    {
+                        Console.WriteLine("Введите тип ремонтной работы:");
+                        string jobType = Console.ReadLine();
+
+                        repairShop.AddRepairJob(new RepairJob { Date = date, JobType = jobType });
+                        Console.WriteLine("Ремонтная работа добавлена!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Некорректный формат даты. Попробуйте снова.");
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+
+                case "2":
+                    Console.WriteLine("Введите начальную дату периода (в формате ГГГГ-ММ-ДД):");
+                    if (DateTime.TryParse(Console.ReadLine(), out DateTime startDate))
+                    {
+                        Console.WriteLine("Введите конечную дату периода (в формате ГГГГ-ММ-ДД):");
+                        if (DateTime.TryParse(Console.ReadLine(), out DateTime endDate))
+                        {
+                            int totalCompletedJobs = repairShop.GetTotalCompletedJobs(startDate, endDate);
+                            Console.WriteLine($"Общее количество выполненных работ за период: {totalCompletedJobs}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Некорректный формат даты. Попробуйте снова.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Некорректный формат даты. Попробуйте снова.");
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+
+                case "3":
+                    Console.WriteLine("Введите начальную дату периода (в формате ГГГГ-ММ-ДД):");
+                    if (DateTime.TryParse(Console.ReadLine(), out startDate))
+                    {
+                        Console.WriteLine("Введите конечную дату периода (в формате ГГГГ-ММ-ДД):");
+                        if (DateTime.TryParse(Console.ReadLine(), out DateTime endDate))
+                        {
+                            Console.WriteLine("Введите тип ремонтной работы:");
+                            string jobType = Console.ReadLine();
+
+                            double averageJobsPerDay = repairShop.GetAverageJobsPerDay(startDate, endDate, jobType);
+                            Console.WriteLine($"Среднее количество работ в день для типа '{jobType}': {averageJobsPerDay}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Некорректный формат даты. Попробуйте снова.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Некорректный формат даты. Попробуйте снова.");
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+
+                case "4":
+                    Console.WriteLine("Введите начальную дату периода (в формате ГГГГ-ММ-ДД):");
+                    if (DateTime.TryParse(Console.ReadLine(), out startDate))
+                    {
+                        Console.WriteLine("Введите конечную дату периода (в формате ГГГГ-ММ-ДД):");
+                        if (DateTime.TryParse(Console.ReadLine(), out DateTime endDate))
+                        {
+                            string mostPopularJobType = repairShop.GetMostPopularJobType(startDate, endDate);
+                            Console.WriteLine($"Наиболее востребованный вид работ за период: {mostPopularJobType}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Некорректный формат даты. Попробуйте снова.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Некорректный формат даты. Попробуйте снова.");
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+
+                case "5":
+                    Console.WriteLine("Вывод списка всех добавленных работ:");
+                    var allRepairJobs = repairShop.GetAllRepairJobs();
+                    foreach (var repairJob in allRepairJobs)
+                    {
+                        Console.WriteLine($"Дата: {repairJob.Date}, Тип работы: {repairJob.JobType}");
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+
+                case "6":
+                    Console.WriteLine("Выход из приложения...");
+                    return;
+
+                default:
+                    Console.WriteLine("Некорректный выбор действия. Попробуйте снова.");
+                    break;
+            }
+        }
+    }
+}
---
+++ LAB6/ConsoleApp6/RepairJobClass/Repair.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RepairShopLibrary
+{
+    /// <summary>
+    /// Представляет задание по ремонту.
+    /// </summary>
+    public class RepairJob
+    {
+        /// <summary>
+        /// Дата выполнения задания.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Тип задания.
+        /// </summary>
+        public string JobType { get; set; }
+
+        /// <summary>
+        /// Представляет сервис ремонтной мастерской.
+        /// </summary>
+        public class RepairShop
+        {
+            private readonly List<RepairJob> repairJobs;
+
+            /// <summary>
+            /// Создает экземпляр класса RepairShop.
+            /// </summary>
+            public RepairShop()
+            {
+                repairJobs = new List<RepairJob>();
+            }
+
+            public List<RepairJob> GetAllRepairJobs()
+            {
+                return repairJobs;
+            }
+
+            /// <summary>
+            /// Добавляет задание по ремонту в сервис.
+            /// </summary>
+            /// <param name="repairJob">Задание по ремонту.</param>
+            public void AddRepairJob(RepairJob repairJob)
+            {
+                repairJobs.Add(repairJob);
+            }
+
+            /// <summary>
+            /// Возвращает общее количество выполненных заданий по ремонту в заданный период времени.
+            /// </summary>
+            /// <param name="startDate">Дата начала периода.</param>
+            /// <param name="endDate">Дата окончания периода.</param>
+            /// <returns>Общее количество выполненных заданий.</returns>
+            public int GetTotalCompletedJobs(DateTime startDate, DateTime endDate)
+            {
+                int count = 0;
+                foreach (var job in repairJobs)
+                {
+                    if (job.Date >= startDate && job.Date <= endDate)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+
+            /// <summary>
+            /// Возвращает среднее количество заданий по ремонту в день для заданного типа задания в заданный период времени.
+            /// </summary>
+            /// <param name="startDate">Дата начала периода.</param>
+            /// <param name="endDate">Дата окончания периода.</param>
+            /// <param name="jobType">Тип задания.</param>
+            /// <returns>Среднее количество заданий в день.</returns>
+            public double GetAverageJobsPerDay(DateTime startDate, DateTime endDate, string jobType)
+            {
+                int totalDays = (endDate - startDate).Days + 1;
+                int jobCount = 0;
+                foreach (var job in repairJobs)
+                {
+                    if (job.Date >= startDate && job.Date <= endDate && job.JobType == jobType)
+                    {
+                        jobCount++;
+                    }
+                }
+                return (double)jobCount / totalDays;
+            }
+
+            /// <summary>
+            /// Возвращает наиболее популярный тип задания по ремонту в заданный период времени.
+            /// </summary>
+            /// <param name="startDate">Дата начала периода.</param>
+            /// <param name="endDate">Дата окончания периода.</param>
+            /// <returns>Наиболее популярный тип задания.</returns>
+            public string GetMostPopularJobType(DateTime startDate, DateTime endDate)
+            {
+                Dictionary<string, int> jobTypeCounts = new Dictionary<string, int>();
+                foreach (var job in repairJobs)
+                {
+                    if (job.Date >= startDate && job.Date <= endDate)
+                    {
+                        if (jobTypeCounts.ContainsKey(job.JobType))
+                        {
+                            jobTypeCounts[job.JobType]++;
+                        }
+                        else
+                        {
+                            jobTypeCounts[job.JobType] = 1;
+                        }
+                    }
+                }
+                string mostPopularJobType = null;
+                int maxCount = 0;
+                foreach (var kvp in jobTypeCounts)
+                {
+                    if (kvp.Value > maxCount)
+                    {
+                        maxCount = kvp.Value;
+                        mostPopularJobType = kvp.Key;
+                    }
+                }
+                return mostPopularJobType;
+            }
+        }
+    }
+}
